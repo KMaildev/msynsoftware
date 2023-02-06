1235571d7e9515193c3259384fab0984382b4556
KMail <kmailsoftwaredevelopment@gmail.com>
2023-02-07 00:47:02 +0630

    Contract & Labour

--- database/sql/msynsoftware (1).sql
+++ database/sql/msynsoftware (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jan 30, 2023 at 07:08 PM
+-- Generation Time: Feb 06, 2023 at 07:12 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 7.4.28
 
@@ -55,6 +55,23 @@ INSERT INTO `agent_lists` (`id`, `name`, `email`, `phone`, `address`, `created_a
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `contracts`
+--
+
+CREATE TABLE `contracts` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `contract_date` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `contract_male` int(11) DEFAULT NULL,
+  `contract_female` int(11) DEFAULT NULL,
+  `remark` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `demand_id` int(11) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `countries`
 --
 
@@ -70,7 +87,32 @@ CREATE TABLE `countries` (
 --
 
 INSERT INTO `countries` (`id`, `title`, `created_at`, `updated_at`) VALUES
-(1, 'Thailand', '2023-01-30 10:38:22', '2023-01-30 10:39:38');
+(1, 'Thailand', '2023-01-30 10:38:22', '2023-01-30 10:39:38'),
+(2, 'Malaysia', '2023-02-06 11:41:27', '2023-02-06 11:41:27'),
+(3, 'Japan', '2023-02-06 11:41:32', '2023-02-06 11:41:32');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `demands`
+--
+
+CREATE TABLE `demands` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `company_name` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `countrie_id` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `overseas_agencie_id` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `male` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `female` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `demand_date` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `cabinet_date` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `issue_date` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `issue_number` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `demand_status` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `demand_id` text COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -86,6 +128,34 @@ CREATE TABLE `failed_jobs` (
   `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
   `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
   `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `labour_management`
+--
+
+CREATE TABLE `labour_management` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `demand_id` int(11) DEFAULT NULL,
+  `contract_id` int(11) DEFAULT NULL,
+  `overseas_agencies_id` int(11) DEFAULT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `father_name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `nrc` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `date_of_birth` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `passport` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `passport_date` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `phone` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `address` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `gender` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `remark` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `place_of_passport` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `owic` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `owic_date` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -112,7 +182,35 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (5, '2023_01_30_162400_add_fields_to_users_table', 2),
 (6, '2023_01_30_170326_create_countries_table', 3),
 (7, '2023_01_30_171039_create_agent_lists_table', 4),
-(8, '2023_01_30_173319_create_overseas_agencies_table', 5);
+(8, '2023_01_30_173319_create_overseas_agencies_table', 5),
+(9, '2023_02_06_040420_create_passports_table', 6),
+(10, '2023_02_06_122630_create_demands_table', 7),
+(11, '2023_02_06_123519_create_offices_table', 8),
+(12, '2023_02_06_134425_create_contracts_table', 9),
+(13, '2023_02_06_142050_add_demand_id_to_demands_table', 10),
+(14, '2023_02_06_162003_create_labour_management_table', 11);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `offices`
+--
+
+CREATE TABLE `offices` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `location` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `offices`
+--
+
+INSERT INTO `offices` (`id`, `name`, `location`, `created_at`, `updated_at`) VALUES
+(1, 'MSYN', 'Yangon', NULL, NULL),
+(2, 'MPS', 'Yangon', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -161,8 +259,38 @@ INSERT INTO `overseas_agencies` (`id`, `company_name`, `type_of_company`, `compa
 (36, 'PROJECT PERMATA JAYA SDN BHD', 'ဆောက်လုပ်ရေးလုပ်ငန်း', NULL, NULL, 'CHERAS, SELANGOR', '2', '2022-12-14 22:04:06', '2022-12-14 22:04:06', 'Mr.LEE'),
 (37, 'GREEN CONCERNS SDN BHD', 'ကားဟိုက်ဒ‌ရောလစ်များတပ်ဆင်သည့်လုပ်ငန်း', NULL, NULL, 'SUNGAI INDAH, SELANGOR', '2', '2022-12-14 22:13:34', '2022-12-14 22:13:34', 'Mr.LEE'),
 (38, 'ACTAN (MALAYSIA) SDN BHD', 'စက္ကူစက်ရုံ', NULL, NULL, 'SELANGOR', '2', '2022-12-14 22:23:44', '2022-12-14 22:23:44', 'KO THAN KYINE'),
-(39, 'GLOBALLINK METAL', 'ဆေးရုံသုံးကုတင်များထုတ်လုပ်သည့်လုပ်ငန်း', NULL, NULL, 'SERI KEMBANGAN, SELANGOR', '2', '2022-12-14 22:31:50', '2022-12-14 22:34:15', 'Mr.LEE'),
-(40, 'KMail Software', 'Softwaer', '09444161554', 'zin@gmail.com', 'Bangkok', '1', '2023-01-30 11:26:33', '2023-01-30 11:30:36', 'Zin Min Tun');
+(39, 'GLOBALLINK METAL', 'ဆေးရုံသုံးကုတင်များထုတ်လုပ်သည့်လုပ်ငန်း', NULL, NULL, 'SERI KEMBANGAN, SELANGOR', '2', '2022-12-14 22:31:50', '2022-12-14 22:34:15', 'Mr.LEE');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `passports`
+--
+
+CREATE TABLE `passports` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `father_name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `nrc` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `date_of_birth` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `passport` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `passport_date` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `local_agent_name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `phone` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `address` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `gender` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `remark` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `join_date` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `owic` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `owic_date` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `reject_status` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `reject_date` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `reject_reason` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `place_of_passport` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `agent_list_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -238,9 +366,21 @@ ALTER TABLE `agent_lists`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `contracts`
+--
+ALTER TABLE `contracts`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `countries`
 --
 ALTER TABLE `countries`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `demands`
+--
+ALTER TABLE `demands`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -251,15 +391,33 @@ ALTER TABLE `failed_jobs`
   ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
 
 --
+-- Indexes for table `labour_management`
+--
+ALTER TABLE `labour_management`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `migrations`
 --
 ALTER TABLE `migrations`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `offices`
+--
+ALTER TABLE `offices`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `overseas_agencies`
 --
 ALTER TABLE `overseas_agencies`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `passports`
+--
+ALTER TABLE `passports`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -291,13 +449,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `agent_lists`
 --
 ALTER TABLE `agent_lists`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `contracts`
+--
+ALTER TABLE `contracts`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `countries`
 --
 ALTER TABLE `countries`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `demands`
+--
+ALTER TABLE `demands`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `failed_jobs`
@@ -306,16 +476,34 @@ ALTER TABLE `failed_jobs`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `labour_management`
+--
+ALTER TABLE `labour_management`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `offices`
+--
+ALTER TABLE `offices`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `overseas_agencies`
 --
 ALTER TABLE `overseas_agencies`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+
+--
+-- AUTO_INCREMENT for table `passports`
+--
+ALTER TABLE `passports`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `personal_access_tokens`
